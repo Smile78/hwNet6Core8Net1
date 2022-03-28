0569d6e51021d895644745cd72119a834beede21
Smile78 <dronussr@gmail.com>
2022-03-28 16:20:11 +0300

    add try with sources

--- src/main/java/netology/Client/JrClient.java
+++ src/main/java/netology/Client/JrClient.java
@@ -7,33 +7,26 @@ public class JrClient {
 
     public static void main(String[] args) throws IOException {
 
-//        Socket clientSocket = new Socket("127.0.0.1", 80);
-        Socket clientSocket = new Socket("netology.homework", 80);
+        try (
+                Socket clientSocket = new Socket("netology.homework", 80);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+            System.out.println("Наберите connection или любой текст )");
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            while (true) {
+                String mssg = reader.readLine();
+                out.write(mssg + "\n");
+                out.flush();
 
-
-        System.out.println("Наберите connection или любой текст )");
-
-        while (true) {
-
-
-            String mssg = reader.readLine();
-
-
-            out.write(mssg + "\n");
-            out.flush();
-
-            if (mssg.equals(("EXIT").toLowerCase())) break;
-
-            String serverWord = in.readLine();
-
-            System.out.println(serverWord);
-
+                if (mssg.equals(("EXIT").toLowerCase())) break;
+                String serverWord = in.readLine();
+                System.out.println(serverWord);
+            }
+        } catch (IOException ex) {
+            System.out.println("sout " + ex);
         }
-
 
     }
 }
